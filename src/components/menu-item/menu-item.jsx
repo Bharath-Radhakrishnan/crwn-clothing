4848c747d86a0fd49788285eebfb8b79024b1028
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-01-16 13:05:25 +0530

    add menu items hovering effects

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./menu-item.scss";
 function MenuItem({ title, img, size }) {
   return (
-    <div
-      className={`menu-item ${size}`}
-      style={{ backgroundImage: `url(${img})` }}
-    >
+    <div className={`menu-item ${size}`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${img})` }}
+      ></div>
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>

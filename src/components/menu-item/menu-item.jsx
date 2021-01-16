f00e4ddb981164ae07d3d66e9781fe47e35545ac
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-01-16 12:19:10 +0530

    add size property and style

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./menu-item.scss";
-function MenuItem({ title, img }) {
+function MenuItem({ title, img, size }) {
   return (
-    <div className="menu-item" style={{ backgroundImage: `url(${img})` }}>
+    <div
+      className={`menu-item ${size}`}
+      style={{ backgroundImage: `url(${img})` }}
+    >
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">SHOP NOW</span>

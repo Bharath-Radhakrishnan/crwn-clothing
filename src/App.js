4aa72e1aa1360cbcee6a1d974546438f80add030
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-01-24 15:59:05 +0530

    add browser routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage";
 
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>HatsPage</h1>
+    </div>
+  );
+};
 function App() {
   return (
     <div className="">
-      <Homepage />
+      <Router>
+        <Switch>
+          <Route exact path="/hats" component={HatsPage}></Route>
+          <Route exact path="/" component={Homepage}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }

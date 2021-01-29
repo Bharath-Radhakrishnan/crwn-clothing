e95d9a99d8bdf086268b7e90454f3328f3d464d8
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-01-29 11:38:21 +0530

    add shop page route and rm dummy hats page route

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shopepage";
 
-const HatsPage = (props) => {
-  return (
-    <div>
-      <h1>HatsPage</h1>
-    </div>
-  );
-};
 function App() {
   return (
     <div className="">
       <Router>
         <Switch>
-          <Route exact path="/hats" component={HatsPage}></Route>
+          <Route exact path="/shop" component={ShopPage}></Route>
           <Route exact path="/" component={Homepage}></Route>
         </Switch>
       </Router>

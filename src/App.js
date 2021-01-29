aee0b42d6fa0f475d7017af4623f70363f6b58ba
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-01-29 16:18:38 +0530

    add sign in and sign up page route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shopepage";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page";
+
 import Header from "./components/header/header";
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/shop" component={ShopPage}></Route>
+          <Route exact path="/signin" component={SignInAndSignUpPage}></Route>
           <Route exact path="/" component={Homepage}></Route>
         </Switch>
       </Router>

cded7da8b57406dfdce27e5802ae9fd9fa7bf916
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-01-31 21:41:33 +0530

    add firbase utils

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDN1nc0KodUrCdbXQR6shiA0DK2s8r1DXU",
+  authDomain: "crwn-db-c423b.firebaseapp.com",
+  projectId: "crwn-db-c423b",
+  storageBucket: "crwn-db-c423b.appspot.com",
+  messagingSenderId: "617338933707",
+  appId: "1:617338933707:web:9dd11161d4aa8d1dee264e",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GithubAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;

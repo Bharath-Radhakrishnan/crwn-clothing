b7d520b71eadf9fe66bd4ea3676f5787e23aa2d2
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-02-17 22:59:21 +0530

    change file name to actionTypes from types

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,5 +1,5 @@
-import { userTypes } from "./user.types";
+import { userActionTypes } from "./user.types";
 export const setCurrentUser = (user) => ({
-  type: userTypes.SET_CURRENT_USER,
+  type: userActionTypes.SET_CURRENT_USER,
   payload: user,
 });

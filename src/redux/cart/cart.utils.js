440a1a3352cd762b2bec8fc38805066ae986daa9
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-03-16 23:50:18 +0530

    remove unused logs

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -2,14 +2,12 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   const cartItemExists = cartItems.find(
     (cartItem) => cartItem.id === cartItemToAdd.id
   );
-  console.log("exists", cartItemExists);
   if (cartItemExists) {
-    console.log("yes");
     return cartItems.map((cartItem) =>
       cartItem.id === cartItemToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
     );
-  } else console.log("noooo");
+  }
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };

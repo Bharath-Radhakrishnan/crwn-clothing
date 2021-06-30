7e518aa2e5027dffdaddadb85a06d2ed9eecf2d1
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-06-30 14:51:54 +0530

    add checkout item key

--- src/pages/checkout/checkoutpage.jsx
+++ src/pages/checkout/checkoutpage.jsx
@@ -25,7 +25,7 @@ const CheckoutPage = ({ cartItems, cartTotal }) => {
         </div>
       </div>
       {cartItems.map((item) => (
-        <CheckoutItem cartItem={item} />
+        <CheckoutItem key={item.id} cartItem={item} />
       ))}
       <div className="total">
         <span>TOTAL : ${cartTotal}</span>

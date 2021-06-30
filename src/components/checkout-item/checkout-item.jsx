26b65f6d3a3b59a39dbe730926d2668d15313829
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-06-30 14:52:21 +0530

    add rmv item fxn

--- src/components/checkout-item/checkout-item.jsx
+++ src/components/checkout-item/checkout-item.jsx
@@ -1,6 +1,9 @@
+import { connect } from "react-redux";
+import { removeItem } from "../../redux/cart/cart.actions";
 import "./checkout-item.scss";
 
-function CheckoutItem({ cartItem: { imageUrl, name, quantity, price } }) {
+function CheckoutItem({ cartItem, removeItem }) {
+  const { id, imageUrl, name, quantity, price } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
@@ -10,9 +13,19 @@ function CheckoutItem({ cartItem: { imageUrl, name, quantity, price } }) {
       <span className="name">{name}</span>
       <span className="quantity">{quantity}</span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div
+        className="remove-button"
+        onClick={() => {
+          removeItem(cartItem);
+        }}
+      >
+        &#10005;
+      </div>
     </div>
   );
 }
-
-export default CheckoutItem;
+const mapStateToProps = () => {};
+const mapDispatchToProps = (dispatch) => ({
+  removeItem: (item) => dispatch(removeItem(item)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CheckoutItem);
